b3d3884caa4e5eb731a3b3fbb1bc8aca4d3cde6a
Gabriel Barros <gabrielbarros@gabriels-mbp.fios-router.home>
2018-02-23 22:37:22 -0500

    Initial Commit

---
+++ hw3_1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hw3_1
+//
+//  Created by Gabriel Barros on 2/23/18.
+//  Copyright © 2018 Gabriel Barros. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
